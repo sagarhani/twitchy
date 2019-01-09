6292a2209c648614014635f3fbf0d771925f6ca9
sagarhani <sagarhani33@gmail.com>
2019-01-09 22:08:49 +0530

    Updates

---
+++ precache-manifest.88cd4b9e25210e3b778159b41b4796ce.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "532638f8d777dce6250f",
+    "url": "/twitchy/static/js/runtime~main.532638f8.js"
+  },
+  {
+    "revision": "2c2b96aaa271e40a9bd9",
+    "url": "/twitchy/static/js/main.2c2b96aa.chunk.js"
+  },
+  {
+    "revision": "191ecdf3eb6e710725b7",
+    "url": "/twitchy/static/js/1.191ecdf3.chunk.js"
+  },
+  {
+    "revision": "891c5da6cee7ca15e22ed237b3aa9426",
+    "url": "/twitchy/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/twitchy/precache-manifest.88cd4b9e25210e3b778159b41b4796ce.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/twitchy/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
